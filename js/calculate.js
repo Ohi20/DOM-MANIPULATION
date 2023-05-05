c65e1f704883cad672fa9ac4ef4401960f4cdd5f
Ohi20 <ashrafohi20@gmail.com>
2023-05-05 13:23:20 +0600

    calculate.js

--- js/calculate.js
+++ js/calculate.js
@@ -15,17 +15,6 @@ document.getElementById("calculate-button").addEventListener('click',function(){
    const incomeInput = document.getElementById("income-input");
    const incomeMoney = incomeInput.value;
 
-    // checking if expenses is greater than income
-    // if(incomeMoney < ((foodMoney+rentMoney+clothesMoney))
-    //     console.log("what");
-    // }
-    // checking if input value is stringd
-    // if(typeof(incomeInput.value,foodInput.value,rentInput.value,clothesInput.value) === 'string' ){
-    //     const successMessage = document.getElementById('notify-success');
-    // const failError = document.getElementById('notify-fail');
-    //     successMessage.style.display = 'none';
-    //     failError.style.display = 'block';
-    //     }
     //calculating total expenses
 
    const totalExpenses = parseFloat(foodMoney) + parseFloat(rentMoney) + parseFloat(clothesMoney);
@@ -50,52 +39,6 @@ document.getElementById("calculate-button").addEventListener('click',function(){
    incomeInput.value = "";
 })
 
-document.getElementById("save-button").addEventListener('click',function(){
-   // get income value
 
-   const incomeInput = document.getElementById("income-input");
-   const incomeMoney = incomeInput.value;
 
-   // get expenses values
-   const foodInput = document.getElementById("food-input");
-   const foodMoney = foodInput.value;
-   const rentInput = document.getElementById("rent-input");
-   const rentMoney = rentInput.value;
-   const clothesInput = document.getElementById("clothes-input");
-   const clothesMoney = clothesInput.value;
-
-   // total expenses
-   const totalExpenses = parseFloat(foodMoney) + parseFloat(rentMoney) + parseFloat(clothesMoney);
-
-   //get current total expense value
-   const expensesTotal = document.getElementById("expenses-total");
-   const expensesTotalText = expensesTotal.innerText;
-   expensesTotal.innerText = parseFloat(totalExpenses);
-
-   // get balance
-    
-   const balanceTotal = document.getElementById("balance-total");
-   balanceTotalText = balanceTotal.innerText;
-   balanceTotal.innerText = incomeMoney - parseFloat(totalExpenses);
-
-   //  find Saving
-
-   const bTotal = balanceTotal.innerText;
-   const save = bTotal * (20/100);
-
-   // Saving amount
-   const totalSaving = document.getElementById("total-saving");
-   const totalSavingText = totalSaving.innerText;
-   totalSaving.innerText = save;
-
-   //  remaining balance
-   const remainingBalance = document.getElementById("remaining-balance");
-   const remainingBalanceText = remainingBalance.innerText;
-   remainingBalance.innerText = bTotal - save;
-
-   //  clear input field
-   foodInput.value = '';
-   rentInput.value = '';
-   clothesInput.value = '';
-   incomeInput.value = '';
-})
+ 
